49c13278c7565f91daf9e3a141327f775f95fb61
aktiwari <aktiwari@nextgen.com>
2020-09-14 15:10:34 +0530

    [refactoring the method]

--- client/src/request.js
+++ client/src/request.js
@@ -1,12 +1,7 @@
 const endpointURL = 'http://localhost:9000/graphql';
+
 export async function loadJobs() {
-   const response = await fetch(endpointURL, {
-        method: 'POST',
-        headers: {
-            'content-type': 'application/json'   
-        },
-        body: JSON.stringify({
-            query:`
+   const query = `
                 query {
                     jobs {
                     id,
@@ -18,22 +13,14 @@ export async function loadJobs() {
                     }
                     } 
                 }                
-                `
-        })
-    });
-    const responseBody = await response.json();
-    return responseBody.data.jobs;
+                `;
+    const {jobs} = await graphqlRequest(query);
+    return jobs;
 }
 
 
 export async function loadJobsById(id) {
-    const response = await fetch(endpointURL, {
-         method: 'POST',
-         headers: {
-             'content-type': 'application/json'   
-         },
-         body: JSON.stringify({
-             query:`
+    const query =`
              query JobQuery($id: ID!) {
                 job(id: $id){
                  id,
@@ -45,10 +32,20 @@ export async function loadJobsById(id) {
                  }
                }
              }              
-                 `,
-            variables: {id}     
-         })
+                 `;
+     const {job} = await graphqlRequest(query,{id});
+     return job;
+ }
+
+
+ export async function graphqlRequest(query, variables={}) {
+    const response = await fetch(endpointURL, {
+         method: 'POST',
+         headers: {
+             'content-type': 'application/json'   
+         },
+         body: JSON.stringify({query,variables})
      });
      const responseBody = await response.json();
-     return responseBody.data.job;
+     return responseBody.data;
  }
