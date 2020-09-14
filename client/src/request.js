ad6c24264c4ef08e8e74ef17c70c8c6774d091a5
aktiwari <aktiwari@nextgen.com>
2020-09-14 12:16:29 +0530

    fetching job by id

---
+++ client/src/request.js
@@ -0,0 +1,54 @@
+const endpointURL = 'http://localhost:9000/graphql';
+export async function loadJobs() {
+   const response = await fetch(endpointURL, {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json'   
+        },
+        body: JSON.stringify({
+            query:`
+                query {
+                    jobs {
+                    id,
+                    title,
+                    company {
+                        id,
+                        name,
+                        description
+                    }
+                    } 
+                }                
+                `
+        })
+    });
+    const responseBody = await response.json();
+    return responseBody.data.jobs;
+}
+
+
+export async function loadJobsById(id) {
+    const response = await fetch(endpointURL, {
+         method: 'POST',
+         headers: {
+             'content-type': 'application/json'   
+         },
+         body: JSON.stringify({
+             query:`
+             query JobQuery($id: ID!) {
+                job(id: $id){
+                 id,
+                 title,
+                 description,
+                 company{
+                   id,
+                   name
+                 }
+               }
+             }              
+                 `,
+            variables: {id}     
+         })
+     });
+     const responseBody = await response.json();
+     return responseBody.data.job;
+ }
